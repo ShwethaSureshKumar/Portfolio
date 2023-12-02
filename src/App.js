a93976317d72f31ff4e803352a1cd3884892f88e
Shwetha S <102687575+ShwethaSureshKumar@users.noreply.github.com>
2023-12-02 21:32:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./App.css";
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+
+const App = () => {
+  return(
+    <>
+    <Header/>
+
+    <main className='main'>
+      <Home />
+      <About />
+      <Skills />
+      <Services />
+    </main>
+    </>
+    
+  )
+}
+
+export default App
