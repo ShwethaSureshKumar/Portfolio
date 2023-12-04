663decd1b1748d5d6445c3fe526a6b23aca43b6c
Shwetha S <102687575+ShwethaSureshKumar@users.noreply.github.com>
2023-12-04 21:50:45 +0530

    Add files via upload
    completed skills,services,qualification and contact
    
    need to start on footer

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react';
+import "./services.css";
 
 const Services = () => {
+    const[toggleState,setToggleState] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
   return (
     <section className="services section" id="services">
         <h2 className="section__title">Services</h2>
@@ -9,23 +16,151 @@ const Services = () => {
         <div className="services__container container grid">
             <div className="services__content">
                 <div>
-                    <i className="uil uil services__icon"></i>
-                    <h3 className="services__title"></h3>
+                    <i className="uil uil-web-grid services__icon"></i>
+                    <h3 className="services__title">
+                        Web <br /> Developer
+                    </h3>
                 </div>
 
-                <span className="services__button">View More
-                <i className="uil uil-arrow-right"></i>
+                <span className="services__button" onClick={() => toggleTab(1)}>
+                    View More
+                <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className= {toggleState===1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i  onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
 
-                        <h3 className="service__modal-title"></h3>
-                        <p className="services__modal-description"></p>
+                        <h3 className="services__modal-title">Web <br /> Developer</h3>
+                        <p className="services__modal-description">Dummy data 
+                        as of now. Will fill when I actually start going to work 
+                        and start providing services.</p>
+
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Web page development.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Design and mockup of products.
+                                </p>
+                            </li>
+
+                        </ul>
                     </div>
                 </div>
             </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-arrow services__icon"></i>
+                    <h3 className="services__title">DSA <br /> Projects</h3>
+                </div>
+
+                <span className="services__button" onClick={() => toggleTab(2)}>
+                    View More
+                <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+                <div className={toggleState===2 ? "services__modal active-modal" : "services__modal"}>
+                    <div className="services__modal-content">
+                        <i  onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+                        <h3 className="services__modal-title">
+                            DSA <br /> Projects
+                        </h3>
+                        <p className="services__modal-description">Dummy data 
+                        as of now. Will fill when I actually start going to work 
+                        and start providing services.</p>
+
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Web page development.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Design and mockup of products.
+                                </p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-edit services__icon"></i>
+                    <h3 className="services__title">
+                        Python/Java <br/> Projects
+                    </h3>
+                </div>
+
+                <span className="services__button" onClick={() => toggleTab(3)}>
+                    View More
+                <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+                <div className={toggleState===3 ? "services__modal active-modal" : "services__modal"}>
+                    <div className="services__modal-content">
+                        <i  onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+                        <h3 className="services__modal-title">Python/Java <br/> Projects</h3>
+                        <p className="services__modal-description">Dummy data 
+                        as of now. Will fill when I actually start going to work 
+                        and start providing services.</p>
+
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Web page development.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Design and mockup of products.
+                                </p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </section>
   )
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ import Libraries from './Libraries';
 
 const Skills = () => {
     return (
-        <section className="skills section">
+        <section className="skills section" id="skills">
             <h2 className="section__title">Skills</h2>
             <span className="section__subtitle">My technical level</span>
 
